865f174f3db8c75f7e714686657d713bd659ac5c
ThaliaBto <sousa_thalia@live.com>
2022-10-29 09:39:01 -0300

    doc: Added folders with bottom bar and option cards widgets

---
+++ lib/widgets/option_cards/OptionsCards.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+Widget optionsCards () {
+  return  Row(
+    mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          card(
+              icon: const Icon(Icons.home),
+              text:'CHAVES PIX'
+          ),
+          const SizedBox(width: 4),
+          card(
+              icon: const Icon(Icons.receipt_long),
+              text:'COMPROVANTES'
+          ),
+          const SizedBox(width: 4),
+          card(
+              icon: const Icon(Icons.home),
+              text:'METAS'
+          ),
+          const SizedBox(width: 4),
+          card(
+              icon: const Icon(Icons.calendar_month),
+              text:'CALENDÁRIO'
+          ),
+        ],
+      );
+
+}
+
+Widget card ({
+  required String text,
+  required Icon icon,
+}) {
+  return SizedBox(
+    width: 84,
+    child: Column(
+      children: [
+
+           ElevatedButton(
+            style: ElevatedButton.styleFrom(
+            fixedSize: const Size(70,70),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(16.0),
+            ),
+            backgroundColor: const Color (0XFFF6F6F6),
+            foregroundColor: const Color (0xFF2F595B),
+          ),
+           child: icon,
+           onPressed: () {},
+        ),
+          const SizedBox(height:8),
+             Text(text,style: const TextStyle (
+            fontFamily:'Inter',
+            fontWeight:FontWeight.w500,
+            color: Color (0XFFF6F6F6),
+            fontSize: 10),
+        ),
+    ],
+    ),
+  );
+}
+
+
+
