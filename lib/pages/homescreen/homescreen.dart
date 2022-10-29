ce3f53ccde63745b5f9865bf391314b1b1efb009
Nicolas <ncn.freedev@gmail.com>
2022-10-29 08:40:39 -0300

    Update alignment

--- lib/pages/homescreen/homescreen.dart
+++ lib/pages/homescreen/homescreen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../../widgets/profile_bar/profilebar.dart';
-import '../cash_balance_bar/cash_balance_bar.dart';
+import '../../widgets/cash_balance_bar/cash_balance_bar.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -16,17 +16,12 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       body: SingleChildScrollView(
         child: Column(
-          children: [
-            const SizedBox(
+          children: const [
+            SizedBox(
               height: 16.0,
             ),
-            const ProfileBar(),
-            Container(
-              color: const Color(0xff2f595b),
-              height: 1.5,
-              width: MediaQuery.of(context).size.width * 0.92,
-            ),
-            const CashBalanceBar(),
+            ProfileBar(),
+            CashBalanceBar(),
           ],
         ),
       ),
