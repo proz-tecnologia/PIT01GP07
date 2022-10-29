9dc3695f7701bedba463c5a5d100f83cc402d4a6
Nicolas <ncn.freedev@gmail.com>
2022-10-28 22:40:33 -0300

    refactor: Update test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:pit01gp07/main.dart';
+import 'package:pit01gp07/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
