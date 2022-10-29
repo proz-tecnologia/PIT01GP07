f420cea1ccba4c779e06dc51244214a541929197
Nicolas <ncn.freedev@gmail.com>
2022-10-29 08:09:16 -0300

    refactor: Creating design_system folder with AppColors class

---
+++ lib/design_system/colors.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+class AppColors {
+  AppColors._();
+
+  static const Color backgroudColor = Color(0xff5cb3b9);
+  static const Color primarytextColor = Color(0xffffffff);
+  static const Color primaryIconColor = Color(0xffffffff);
+}
