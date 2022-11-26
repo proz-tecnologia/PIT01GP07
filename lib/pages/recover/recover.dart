b7c84fe348dd933203e301495813f5e28a7e804f
ThaliaBto <sousa_thalia@live.com>
2022-11-26 10:24:54 -0300

    feat: Added email validation and dialog box to Recover Page

--- lib/pages/recover/recover.dart
+++ lib/pages/recover/recover.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../../widgets/dialog_box/custom_dialog_box.dart';
 import '../email_confirmation/email_confirmation .dart';
 
 class RecoverScreen extends StatefulWidget {
@@ -8,13 +9,17 @@ class RecoverScreen extends StatefulWidget {
   State<RecoverScreen> createState() => _RecoverScreen();
 }
 class _RecoverScreen extends State<RecoverScreen> {
+  final _formKey = GlobalKey<FormState>();
+  final _emailController = TextEditingController();
    @override
   Widget build(BuildContext context) {
     return Scaffold(
      resizeToAvoidBottomInset: false,
       body: Padding(
         padding: const EdgeInsets.only(left:16,right:16),
-        child: Column(
+        child: Form(
+             key:_formKey,
+             child: Column(
              children: <Widget>[
               const SizedBox(height:32),
               Align(
@@ -46,19 +51,19 @@ class _RecoverScreen extends State<RecoverScreen> {
                  ),),
               const SizedBox(height:32),
               SizedBox(
-                height:52,
-                child: TextField(
+                child: TextFormField(
+                   controller: _emailController,
                    cursorColor: const Color (0xFF539CA1),
                    decoration: InputDecoration(
+                   counterText:" ",
                    focusColor:  const Color (0xFF539CA1),
-                   enabledBorder: OutlineInputBorder(
+                   border:  OutlineInputBorder(
                    borderRadius: const BorderRadius.all(Radius.circular(12.0)),
                    borderSide: BorderSide(color: const Color(0xFF539CA1).withOpacity(0.4), width: 2),
     ),
                    focusedBorder: const OutlineInputBorder(
-                   borderRadius: BorderRadius.all(Radius.circular(10.0)),
                    borderSide: BorderSide(color: Color (0xFF539CA1),width: 2),),
-                   labelText: 'Digite seu e-mail',
+                   hintText:  'Digite seu e-mail',
                        labelStyle: TextStyle(
                          fontFamily:'Inter',
                          fontWeight:FontWeight.w500,
@@ -72,11 +77,20 @@ class _RecoverScreen extends State<RecoverScreen> {
                        fontSize: 14,
                        color: Color (0xFF2F595B),
                      ),
+                 validator:(email){
+                 if (email==null || email.isEmpty){
+                 return 'E-mail obrigatório';
+                 }else if (!email.contains("@")){
+                  return 'Email inválido';
+                 };
+                 return null;
+                 }
                  ),
               ),
               const SizedBox(height:24),
               ElevatedButton(
                  onPressed: () {
+                   if (_formKey.currentState!.validate()){}
                    Navigator.push(context,MaterialPageRoute(builder:
                    (context)=> const EmailConfirmation()),
                    );
@@ -92,12 +106,38 @@ class _RecoverScreen extends State<RecoverScreen> {
                 ),),
               ),
               const SizedBox(height:96),
-              const Expanded(
+               ElevatedButton(
+                 onPressed: () {
+                   showDialog(context: context,
+                     builder: (BuildContext context)
+                     {
+                       return CustomDialogBox(
+                         title: "Oops!",
+                         descriptions: "Não conseguimos identificar esse endereço de e-mail, mas você pode tentar de novo. ",
+                         text: "Tentar de novo",
+                         img: Image.asset('assets/images/background/erro.png'),
+                       );
+                     },
+                   );
+                 },
+                 style: ElevatedButton.styleFrom(
+                     fixedSize: const Size(200, 48), backgroundColor: const Color (0xFF539CA1)),
+                 child: const Text('MENSAGEM DE ERRO',
+                   style: TextStyle(
+                     fontFamily:'Inter',
+                     fontWeight:FontWeight.w500,
+                     fontSize: 14,
+                     color: Color (0xFFF6F6F6),
+                   ),),
+               ),
+               const SizedBox(height:16),
+               const Expanded(
                 child: Image(image: AssetImage( "assets/images/background/forgot_password.png"),
                  alignment: Alignment.bottomLeft,
                 ),
               ),
         ],
+             ),
         ), // This trailing comma makes auto-formatting nicer for build methods.
     ),
     );
