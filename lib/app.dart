fd1f7a4866873b7602c9f3b2dfd12a5d027b1d63
ThaliaBto <sousa_thalia@live.com>
2022-12-07 13:28:11 -0300

    refactor: update app.dart

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'design_system/colors.dart';
+import 'design_system/light_theme.dart';
 import 'pages/splashscreen/splashscreen.dart';
 
 class MyApp extends StatelessWidget {
@@ -9,18 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        fontFamily: 'Inter',
-        iconTheme: const IconThemeData(
-          color: AppColors.primarytextColor,
-        ),
-        scaffoldBackgroundColor: AppColors.backgroudColor,
-        elevatedButtonTheme: ElevatedButtonThemeData(
-            style: ButtonStyle(
-          backgroundColor: MaterialStateProperty.all(const Color(0xff3b3d53)),
-        )),
-        appBarTheme: const AppBarTheme(backgroundColor: Color(0xff5cb3b9)),
-      ),
+      theme:lightTheme,
       home: const SplashScreen(),
     );
   }
