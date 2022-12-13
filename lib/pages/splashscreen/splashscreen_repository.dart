59bae6e9fe7d678842f6059af1a2a669ecf79d5b
Nicolas <ncn.freedev@gmail.com>
2022-12-13 14:48:38 -0300

    refactor: Solving the first login problem

--- lib/pages/splashscreen/splashscreen_repository.dart
+++ lib/pages/splashscreen/splashscreen_repository.dart
@@ -1,23 +1,18 @@
 import 'package:shared_preferences/shared_preferences.dart';
-import '../login/login_model.dart';
 
 class SplashScreenRepository {
   late SharedPreferences preferences;
 
-  // Future<void> userLogin(LoginModel userModel) async {
-  //   preferences = await SharedPreferences.getInstance();
-  //   preferences.setBool('isLogged', true);
-  //   preferences.setString('lastLogged', userModel.email);
-  // }
-
-  // Future<String> lastLogged() async {
-  //   preferences = await SharedPreferences.getInstance();
-  //   return preferences.getString("lastLogged")!;
-  // }
-
   Future<bool?> islogged() async {
     preferences = await SharedPreferences.getInstance();
 
-    return preferences.getBool('isLogged');
+    bool? islogged = preferences.getBool('isLogged');
+
+    if (islogged == null) {
+      preferences.setBool('islogged', false);
+      return false;
+    } else {
+      return islogged;
+    }
   }
 }
