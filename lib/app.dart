aca8bafc181d76e8cddd8c74868e08d2e4361f1a
Nicolas <ncn.freedev@gmail.com>
2022-12-26 12:47:26 -0300

    refactor: Introducing Flutter Named Routes

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:teste/pages/email_confirmation/email_confirmation.dart';
+import 'package:teste/pages/homescreen/homescreen.dart';
+import 'package:teste/pages/login/login.dart';
+import 'package:teste/pages/new_password/new_password.dart';
+import 'package:teste/pages/recover/recover.dart';
+import 'package:teste/pages/sign_up/sign_up.dart';
 import 'design_system/light_theme.dart';
 import 'pages/splashscreen/splashscreen.dart';
 
@@ -9,14 +15,25 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      localizationsDelegates:const [
+      localizationsDelegates: const [
         GlobalMaterialLocalizations.delegate,
         GlobalWidgetsLocalizations.delegate
       ],
-      supportedLocales: const [ Locale('pt', 'BR')],
+      supportedLocales: const [Locale('pt', 'BR')],
       debugShowCheckedModeBanner: false,
-      theme:lightTheme,
-      home: const SplashScreen(),
+      theme: lightTheme,
+      initialRoute: '/',
+      routes: {
+        // When navigating to the "/" route, build the FirstScreen widget.
+        '/': (context) => const SplashScreen(),
+        // When navigating to the "/second" route, build the SecondScreen widget.
+        '/login': (context) => const LoginScreen(),
+        '/home': (context) => const HomeScreen(),
+        '/signup': (context) => const SignUp(),
+        '/newpassword': (context) => const NewPassword(),
+        '/recover': (context) => const RecoverScreen(),
+        '/emailconfirmation': (context) => const EmailConfirmation()
+      },
     );
   }
 }
