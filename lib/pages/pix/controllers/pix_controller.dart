35682a6900cb370872d7d7c48e22423aff0bd464
pertusatti22 <pertusatti22@gmail.com>
2023-01-17 09:19:53 -0300

    add pix controller and pix repository

---
+++ lib/pages/pix/controllers/pix_controller.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/cupertino.dart';
+import 'package:teste/pages/pix/pix_model.dart';
+
+import '../pix_repository.dart';
+
+
+class PixController {
+  List<PixModel> pix = [];
+  final PixRepository _pixRepository;
+  final state = ValueNotifier(PixState.start);
+
+  PixController([PixRepository? repository])
+      : _pixRepository = repository ?? PixRepository();
+
+  Future start() async {
+    state.value = PixState.loading;
+    try {
+      pix = await _pixRepository.getPix();
+      state.value = PixState.success;
+    } catch (e) {
+      state.value = PixState.error;
+    }
+  }
+}
+
+enum PixState { start, loading, success, error }
